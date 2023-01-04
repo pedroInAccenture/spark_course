993bf4a39fd6f5b73911e5d5a5ee48d3be0646f8
Ortega <pedro.a.ortega@accenture.com>
2023-01-04 13:37:29 -0600

    se agrega ejemplo para spark submit y para la lectura de archivos de configuración

--- src/main/scala/com/course/common/Constant.scala
+++ src/main/scala/com/course/common/Constant.scala
@@ -2,7 +2,7 @@ package com.course.common.Constants
 
 import com.typesafe.config.ConfigFactory
 
-object scala {
+object Constant {
 //  final val appConfig = ConfigFactory.parseFile("appConfigFileName")
 //  final val envConstants = appConfig.getConfig("ENV")
 
---
+++ src/main/scala/com/course/common/LoadConf.scala
@@ -0,0 +1,5 @@
+package com.course.common
+
+object LoadConf {
+
+}
